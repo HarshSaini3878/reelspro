796176d19ec9f6305552a5f9755c9ec22b79d4ee
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-02-06 18:54:10 +0530

    added api in files

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest,NextResponse } from "next/server";
+import { dbConnect } from "@/lib/db";
+export async function POST (req:NextRequest){
+try {
+    
+} catch (error) {
+    
+}
+}
