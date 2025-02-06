8b2eee6efa94ccd46011f7e1c57252a277c0a490
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-02-06 19:01:17 +0530

    user regiister api ready

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,9 +1,43 @@
 import { NextRequest,NextResponse } from "next/server";
 import { dbConnect } from "@/lib/db";
-export async function POST (req:NextRequest){
+import { error } from "console";
+import User from "@/models/User";
+export async function POST (request:NextRequest){
 try {
-    
+    const {email,password}= await request.json();
+    if(!email || !password){
+        return NextResponse.json({
+            error:"email and password required"
+        },{
+           status:400
+        })
+    }
+
+    await dbConnect();
+    const existingUser=await User.findOne({email});
+    if(existingUser){
+        return NextResponse.json({
+            error:"email already registered"
+        },{
+           status:400
+        })
+    }
+
+    const user=await User.create({
+        email,password
+    });
+    await user.save();
+    return NextResponse.json({
+        message:"user succefully registered"
+    },{
+       status:201
+    })
+
 } catch (error) {
-    
+    NextResponse.json({
+        error:"Internal server error"
+    },{
+       status:500
+    })
 }
 }
